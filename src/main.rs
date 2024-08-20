52bcbe587f4240895abe447bd91bd1b8664d0ef6
Adam Vajda <vjdad4m@gmail.com>
2024-08-20 20:44:58 +0200

    feat: implemented GameBoyState with memory and registers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,74 @@
+struct GameBoyRegister {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    f: u8,
+    h: u8,
+    l: u8,
+}
+
+struct GameBoyState {
+    memory: [u8; 0xFFFF],
+    registers: GameBoyRegister,
+    sp: u16,
+    pc: u16,
+}
+
+impl GameBoyState {
+    fn new() -> GameBoyState {
+        GameBoyState {
+            memory: [0; 0xFFFF],
+            registers: GameBoyRegister {
+                a: 0,
+                b: 0,
+                c: 0,
+                d: 0,
+                e: 0,
+                f: 0,
+                h: 0,
+                l: 0,
+            },
+            sp: 0xFFFE,
+            pc: 0x100,
+        }
+    }
+
+    fn load_rom(&mut self, rom: Vec<u8>) {
+        for i in 0..rom.len()-1 {
+            self.memory[i] = rom[i];
+        }
+    }
+
+    fn dump_registers(&self) {
+        println!("A: {:02X}", self.registers.a);
+        println!("B: {:02X}", self.registers.b);
+        println!("C: {:02X}", self.registers.c);
+        println!("D: {:02X}", self.registers.d);
+        println!("E: {:02X}", self.registers.e);
+        println!("F: {:02X}", self.registers.f);
+        println!("H: {:02X}", self.registers.h);
+        println!("L: {:02X}", self.registers.l);
+    }
+
+    fn dump_memory(&self) {
+        for i in 0..0xFFFF {
+            if (i+1) % 32 == 0 {
+                println!();
+            }
+            print!("{:02X} ", self.memory[i]);
+        }
+        println!();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rom_file: &str = "./rom/gb-test-roms/cpu_instrs/cpu_instrs.gb";
+    let rom: Vec<u8> = std::fs::read(rom_file).unwrap();
+    let mut gb = GameBoyState::new();
+    gb.load_rom(rom);
+    
+    gb.dump_memory();
+    gb.dump_registers();
 }
