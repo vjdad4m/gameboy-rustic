a293049904bd31ba29253bd9d0e8b2f6d50b21c6
Adam Vajda <vjdad4m@gmail.com>
2024-08-21 12:02:48 +0200

    feat: setup display function

---
+++ src/display.rs
@@ -0,0 +1,18 @@
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+use sdl2::EventPump;
+
+pub fn setup_display() -> (Canvas<Window>, EventPump) {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("GameBoy Rustic Emulator", 640, 320)
+        .position_centered()
+        .build()
+        .unwrap();
+    let canvas = window.into_canvas().build().unwrap();
+    let event_pump = sdl_context.event_pump().unwrap();
+    (canvas, event_pump)
+}
